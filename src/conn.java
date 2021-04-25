ea519fa03ef1f215d20acfa8ea25790622a3211d
shivkumar2001 <shivkumar1872001@gmail.com>
2021-04-25 09:09:37 +0530

    payroll

---
+++ src/conn.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shiv
+// */
+//public class conn {
+//    
+//}
+import java.sql.*;
+
+public class conn {
+    
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql://localhost:3306/project211","root","");
+            s = c.createStatement();
+            
+        
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
